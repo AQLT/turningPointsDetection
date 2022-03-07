7eb19c05fe8a73ec5224c8d0faa1a64facaa52fd
AQlT <alain.quartier@yahoo.fr>
2022-03-07 13:37:28 +0100

    installation

--- R package/installation_onyxia.R
+++ R package/installation_onyxia.R
@@ -1,7 +1,8 @@
 system("sudo apt update")
+install.packages(c("zoo", "mathjaxr"))
 system("sudo apt install libprotoc-dev libprotobuf-dev protobuf-compiler")
 
-install.packages(c("zoo", "RProtoBuf", "mathjaxr"))
+install.packages("RProtoBuf")
 # devtools::install_github("palatej/rjd3toolkit")
 install.packages("R package/rjd3toolkit_0.1.1.tar.gz", repos = NULL, type = "source")
 devtools::install_github("AQLT/rjdfilters")
