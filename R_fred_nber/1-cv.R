47f91a60b179f808ca62c2572ec3b5e590d3e7f6
aqlt <alain.quartier-la-tente@insee.fr>
2022-04-04 15:17:27 +0200

    more h values

--- R_fred_nber/1-cv.R
+++ R_fred_nber/1-cv.R
@@ -4,16 +4,22 @@ plan(multisession)
 if(!dir.exists("data/byseriescv_nber"))
   dir.create("data/byseriescv_nber")
 
+h_cv <- 18
 for(s in list.files("data/byseries_nber",full.names = TRUE)){
   print(s)
   data <- readRDS(s)
+  
   info_fs <- lapply(data, function(x){
-    future({sapply(3:12, function(i){
+    future({sapply(3:h_cv, function(i){
       tmp = CV(x, i)^2
-      mean(tmp[-c(1:12, 1:12 + length(x) - 12)], na.rm=TRUE)})
+      if(all(is.na(tmp)))
+        return(NA)
+      mean(tmp[-c(1:h_cv, 1:h_cv + length(x) - h_cv)], na.rm=TRUE)
+    }
+    )
     })
   })
   info <- sapply(info_fs, value)
-  rownames(info) <- sprintf("h=%i", 3:12)
+  rownames(info) <- sprintf("h=%i", 3:h_cv)
   saveRDS(info, sprintf("data/byseriescv_nber/%s", basename(s)))
 }
