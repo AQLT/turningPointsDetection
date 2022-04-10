b2798552c8ceaf847ff83749005973620c3bc4f8
AQlT <alain.quartier@yahoo.fr>
2022-04-10 18:14:28 +0200

    bryboschan

---
+++ R_fred_nber/2F-BryBoschan.R
@@ -0,0 +1,54 @@
+if(!dir.exists("results_nber"))
+  dir.create("results_nber")
+if(!dir.exists("results_nber/bp"))
+  dir.create("results_nber/bp")
+library(Coinprofile)
+library(AQLThesis)
+library("future")
+plan(multisession)
+bb_tp = function(x){
+  first_data = start(x)
+  tp = TP_BryBoschan(x, year = first_data[1],
+                     month = first_data[2],frequ = frequency(x))
+  date_tp = time(x)[tp$Time]
+  list(upturn = date_tp[tp$Max_Min == 1],
+       downturn = date_tp[tp$Max_Min == 2])
+}
+
+list_series <- list.files("data/byseries_nber", full.names = TRUE)
+
+fs <- list()
+i <- 0
+
+for(s in list_series){
+  name_file <- gsub(".RDS$", "", basename(s))
+  print(name_file)
+  
+  nom_f_s_tp <- 
+    sprintf("results_nber/bp/%stp.RDS",
+            name_file)
+  
+  if(all(file.exists(nom_f_s_tp)))
+    next;
+  
+  i <- i+1
+  
+  fs[[i]] <- future({
+    print(s)
+    data <- readRDS(s)
+    data_info <- readRDS(sub("byseries_nber", "byseriesinfo_nber", s))
+    
+    tp <- lapply(names(data), function(nom_d){
+      x <- data[[nom_d]]
+      bb_tp(x)
+    })
+    names(tp) <- names(data)
+    
+    saveRDS(tp,
+            nom_f_s_tp)
+    TRUE
+  })
+}
+
+
+vs <- lapply(fs, value)
---
+++ R_simul/2F-BryBoschan.R
@@ -0,0 +1,58 @@
+if(!dir.exists("results_simul"))
+  dir.create("results_simul")
+if(!dir.exists("results_simul/bp"))
+  dir.create("results_simul/bp")
+library(Coinprofile)
+library(AQLThesis)
+library("future")
+plan(multisession)
+bb_tp = function(x){
+  first_data = start(x)
+  tp = TP_BryBoschan(x, year = first_data[1],
+                     month = first_data[2],frequ = frequency(x))
+  date_tp = time(x)[tp$Time]
+  list(upturn = date_tp[tp$Max_Min == 1],
+       downturn = date_tp[tp$Max_Min == 2])
+}
+
+list_series <- list.files("data_simul/byseries", full.names = TRUE)
+s = list_series[1]
+fs <- list()
+j <- 1
+reload <- FALSE
+
+for(s in list_series){
+  name_file <- gsub(".RDS$", "", basename(s))
+  print(name_file)
+  data <- readRDS(s)
+  data_info <- readRDS(sub("byseries", "byseriesinfo", s))
+  nom_f_s <- sprintf("results_simul/bp/%s.RDS",
+                     name_file)
+  nom_f_s_tp <- 
+    sprintf("results_simul/bp/%s_tp.RDS",
+            name_file)
+  
+  if(all(file.exists(nom_f_s_tp)))
+    next;
+  
+  reload <- TRUE
+  fs[[j]] <- future({
+    print(s)
+    tp <- lapply(names(data), function(nom_d){
+      x <- data[[nom_d]]
+      bb_tp(x)
+    })
+    names(tp) <- names(data)
+    
+    saveRDS(tp,
+            nom_f_s_tp)
+    TRUE
+  })
+  j <- j+1
+}
+if(reload){
+  vs <- lapply(fs, value)
+  fs <- list()
+  j <- 1
+}
+
